4f06c9a936cb27f7ab4dd4025a00919142073161
munasarabokar <110159068+munasarabokar@users.noreply.github.com>
2022-07-30 17:35:31 +0300

    Update WrongNetworkMessage.js

--- components/WrongNetworkMessage.js
+++ components/WrongNetworkMessage.js
@@ -1,6 +1,7 @@
 const WrongNetworkMessage = () => <div className='flex flex-col justify-center items-center mb-20 font-bold text-2xl gap-y-3'>
   {/* Prompt to change network to Rinkeby */}
   <div>----------------------------------------</div>
+<title>Error Network</title>
   <div>Please connect to the Rinkeby Testnet</div>
   <div>and reload the page</div>
   <div>----------------------------------------</div>
