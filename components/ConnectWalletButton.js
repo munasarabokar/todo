75a3d229ec8118ddbe915ca71371070fcacb5258
munasarabokar <110159068+munasarabokar@users.noreply.github.com>
2022-07-30 17:32:34 +0300

    Update ConnectWalletButton.js

--- components/ConnectWalletButton.js
+++ components/ConnectWalletButton.js
@@ -1,4 +1,5 @@
 const ConnectWalletButton = ({connectWallet}) =>
+<title>Login Now</title>
   <button
     className='h-[5rem] text-2xl font-bold py-3 px-12 bg-[#f1c232] rounded-lg mb-10 hover:scale-105 transition duration-500 ease-in-out'
   // Add an onClick functionality
